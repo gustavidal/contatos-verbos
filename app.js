d6bc718edcaec0b89536e52e721a0171e9f6a69a
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2026-05-22 20:02:17 -0300

    feat: enviar projeto

--- app.js
+++ app.js
@@ -95,11 +95,15 @@ function criarLinha(contato) {
     foto.appendChild(imagem)
 
     const atualizar = document.createElement('button')
-    atualizar.textContent = 'U'
+    atualizar.className = 'btn-acao btn-editar'
+    atualizar.title = 'Editar contato'
+    atualizar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4Z"/></svg>`
     atualizar.addEventListener('click', () => put(contato))
 
     const deletar = document.createElement('button')
-    deletar.textContent = 'D'
+    deletar.className = 'btn-acao btn-excluir'
+    deletar.title = 'Excluir contato'
+    deletar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/><path d="M10 11v6"/><path d="M14 11v6"/><path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/></svg>`
     deletar.addEventListener('click', () => del(contato.id))
 
     acoes.append(atualizar, deletar)
